df6f227b4e2b34ad80ef7980366dd76727d18bd2
khaledxz150 <k.abulawi@cap-me.com>
2025-06-04 17:52:17 +0300

    ltr support

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+﻿const gulp = require('gulp');
+const rtlcss = require('gulp-rtlcss');
+const rename = require('gulp-rename');
+
+// Task to generate site.rtl.css from site.css
+gulp.task('rtl', function () {
+    return gulp.src('wwwroot/css/site.css')       // Source LTR file
+        .pipe(rtlcss())                              // Convert to RTL
+        .pipe(rename({ suffix: '.rtl' }))            // Rename to site.rtl.css
+        .pipe(gulp.dest('wwwroot/css'));             // Output to the same directory
+});
